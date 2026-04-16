918a1bf172201d9ebcd133fcf926df74fc3ae742
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2026-04-16 19:57:34 +0600

    working with temporary chat

--- src/components/chat-input.tsx
+++ src/components/chat-input.tsx
@@ -25,32 +25,39 @@ const ChatInput = ({ placeholder = "Ask anything.." }: Props) => {
   return (
     <div
       onKeyDown={handleKeyDown}
-      className="border rounded-xl relative min-h-28 max-h-42 max-w-3xl mx-auto"
+      className="border rounded-xl min-h-28 max-h-42 max-w-3xl mx-auto"
     >
       <Textarea
         placeholder={placeholder}
-        className="bg-neutral border-none focus:outline-none focus:ring-0 focus:border-none focus:shadow-none focus-visible:ring-0 placeholder:text-lg placeholder:leading-none p-3 min-h-14 max-h-28 overflow-y-auto"
+        className="bg-neutral border-none focus:outline-none focus:ring-0 focus:border-none focus:shadow-none focus-visible:ring-0 placeholder:text-lg placeholder:leading-none p-3 min-h-20 max-h-36 overflow-y-auto"
         autoFocus
       />
 
-      <div className="">
+      <div className="flex justify-between pr-3 pb-2">
         <Tooltip>
-          <TooltipTrigger className="">
-            <span className="absolute left-3 h-9 w-9 flex items-center justify-center bottom-2 text-muted-foreground">
+          <TooltipTrigger className="pl-2 ">
+            <span className="h-9 w-9 flex items-center justify-center text-muted-foreground">
               <MinusIcon size={24} />
             </span>
           </TooltipTrigger>
-          <TooltipContent>
-            <p>No file insertion is allowed.</p>
+
+          <TooltipContent className="relative">
+            <p className="text-sm">No file insertion is allowed. 😆 </p>
           </TooltipContent>
         </Tooltip>
 
-        <Button
-          type="submit"
-          className="absolute right-4 h-9 w-9 text-xl rounded-full bottom-2"
-        >
-          <ArrowUp size={32} className="h-8 w-8" />
-        </Button>
+        <Tooltip>
+          <TooltipTrigger>
+            <span className="bg-primary text-primary-foreground flex items-center justify-center right-4 h-9 w-9 text-xl rounded-full">
+              <ArrowUp size={24} className="h-6 w-6" />
+            </span>
+          </TooltipTrigger>
+          <TooltipContent side="bottom">
+            <p className="text-sm">
+              Send Prompt. <kbd>↵</kbd>
+            </p>
+          </TooltipContent>
+        </Tooltip>
       </div>
     </div>
   );
